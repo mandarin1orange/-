9e109cdf59d79b3ac1bebc103757bb4e3c43bd7c
mandarin1orange <100086932+mandarin1orange@users.noreply.github.com>
2022-03-17 21:50:20 +0800

    字典生成器
    # 引用 itertools 库

---
+++ tool-20.py
@@ -0,0 +1,17 @@
+import itertools
+
+def main():
+
+    words = '1234567890'
+    temp = itertools.permutations(words,2)
+    passwords = open('dic.txt','a')
+
+    for i in temp:
+        passwords.write(''.join(i))
+        passwords.write(''.join('\n'))
+    passwords.close()
+
+    pass
+
+if __name__ == '__main__':
+    main()
